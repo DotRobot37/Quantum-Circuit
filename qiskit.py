95910da35673662e896d817e8f3b63c22e072569
DotRobot37 <55563688+DotRobot37@users.noreply.github.com>
2020-03-17 10:45:28 -0700

    Create qiskit.py

---
+++ qiskit.py
@@ -0,0 +1,19 @@
+import qiskit as qk
+
+#create qubits
+q=qk.QuantumRegister(2)
+
+#create classic bits
+c=qk.ClassicalRegister(2)
+circuit=qk.QuantumCircuit(q,c)
+
+#Hadamard Gate on first qubit
+circuit.h(q[0])
+
+#CNOT Gate on first and second qubit
+circuit.cx(q[0],q[1])
+
+#measure qubits
+circuit.measure(q,c)
+
+print (circuit)
